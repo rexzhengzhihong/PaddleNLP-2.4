c2b7085f18f80a6c388ed0e1c6f4c54affd7cbb2
onezzhengzh <394207146@qq.com>
2023-01-04 13:44:43 +0800

    uie,运行出来

---
+++ model_zoo/uie/test.py
@@ -0,0 +1,13 @@
+from pprint import pprint
+from paddlenlp import Taskflow
+import sys
+
+def predit():
+    schema = ['出发地', '目的地', '费用', '时间']
+    # 设定抽取目标和定制化模型权重路径
+    my_ie = Taskflow("information_extraction", schema=schema,task_path='/home/DiskA/zncsPython/paddlenlp/uie/checkpoint/model_best')
+    print(my_ie("城市内交通费7月5日金额114广州至佛山"))
+
+if __name__ == '__main__':
+    #print(sys.path)
+    predit()
